42d5069bf62bee2c2d8a03ab9cda822467fb84af
Laurent Toutain <laurent@touta.in>
2026-04-08 11:21:00 +0200

    Create enumeration branch and update SID plugin / VS Code debug config

--- pyang/plugins/sid.py
+++ pyang/plugins/sid.py
@@ -803,6 +803,11 @@ class SidFile:
                         for t in s.i_type_spec.types:
                             if t.i_type_spec.name == "identityref":
                                 typename.insert(0, "identityref") # put identityref first in the list as it is more specific than other types
+                            elif t.i_type_spec.name == "enumeration":
+                                enum_object = {}
+                                for k, v in t.i_type_spec.enums:
+                                    enum_object[str(v)] = k
+                                typename.append(enum_object)
                             else:
                                 typename.append(t.arg)
         self.merge_item('data', self.get_path(statement), typename if self.sid_extension else None)
